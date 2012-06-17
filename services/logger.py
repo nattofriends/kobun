efe1a1f122c49d04b27dfbcaf6d4dc583e2237ad
Tony Young <tony@rfw.name>
2012-06-17 22:56:20 +1200

    add logger

---
+++ services/logger.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+
+from kobunsupport import load_config, handshake, read_line, write_line
+
+import datetime
+
+handshake("legacy-style kobun logger")
+
+config = load_config()
+
+f = open(config["logger.file"], "wa")
+
+while True:
+    server, prefix, command, args = read_line()
+
+    if command.lower() == "privmsg":
+        target, msg = args
+
+        f.write("{} [KobunClient,client] {} <{}> {}\n".format(
+            datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S+0000"), target, prefix, msg
+        ))
+
